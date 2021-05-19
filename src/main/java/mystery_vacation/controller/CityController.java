a4438258d11393349544c9a34d358a135fa2c293
ericahicks <48265629+ericahicks@users.noreply.github.com>
2021-05-19 01:03:58 -0400

    Update CityController.java
    google style guide says no blank lines between imports (except between static an nonstatic imports) https://google.github.io/styleguide/javaguide.html#s3.3.3-import-ordering-and-spacing

--- src/main/java/mystery_vacation/controller/CityController.java
+++ src/main/java/mystery_vacation/controller/CityController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import mystery_vacation.domain.CityInfo;
 import mystery_vacation.service.CityService;
  
